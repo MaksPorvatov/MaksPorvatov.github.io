1513dd856fee433df7e1551b48d0c1bc07cee61d
GarretShadow <maksporvatov@gmail.com>
2015-11-29 21:39:59 +0200

    up

---
+++ GoIT/markup/home/lessonJS_10-11_part2/script/script.js
@@ -0,0 +1,19 @@
+var arr = ['Maks', 'Oleg', 'Lili', 'Nick', 'Lol'];
+
+arr = prompt('Enter your name');
+
+
+if (arr == 'Maks') {
+  alert( 'Hello Maks');
+} else if ( arr == 'Oleg' ) {
+  alert( 'Hello Oleg' );
+} else if ( arr == 'Lili' ) {
+  alert( 'Hello Lili' );
+} else if ( arr == 'Nick' ) {
+  alert( 'Hello Nick' );
+} else if ( arr == 'Lol' ) {
+  alert( 'Hello Lol' );
+} else {
+  alert('fatal error');
+}
+
