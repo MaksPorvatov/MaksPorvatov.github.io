a1382f91c4ed56423ad981b04929f79158b75b44
Maks Porvatov <maksporvatov@gmail.com>
2016-02-29 11:15:02 +0200

    Done

--- GoIT/markup/home/JS/LessonJS 13-14/script/script.js
+++ GoIT/markup/home/JS/LessonJS 13-14/script/script.js
@@ -1,61 +1,165 @@
 'use strict'
 
-var questions=[
-{
-    text: "Как звали крокодила, друга Чебурашки?:",
-    answers: ["Гена",
-          "Генератор",
-          "Ганджубас"],
-    correctAnswer: 0 
-},
-{
-    text: "Как звали черепаху?",
-    answers: ["Тротилла",
-          "Тортилла",
-          "Торчилла"],
-    correctAnswer: 1
-},
-{
-    text: "У тебя все хорошо?:",
-    answers: ["Да!! Все отлично",
-          "Не, не очень.",
-          "Такое"],
-    correctAnswer: 0
-}
-];
+$(function () {
+  
 
-var yourAns = new Array;
-var score = 0;
 
-function Engine(question, answer) {yourAns[question]=answer;}
-
-function Score(){
-   var answerText = "Результаты:\n";
-   for(var i = 0; i < yourAns.length; ++i){
-    var num = i+1;
-    answerText=answerText+"\n    Вопрос №"+ num +"";
-    if(yourAns[i]!=questions[i].correctAnswer){
-        answerText=answerText+"\n    Правильный ответ: " +
-        questions[i].answers[questions[i].correctAnswer] + "\n";
+  var info = [
+    {
+      question : "Как звали черепаху?",
+      answer   : [
+      "Торчила",
+      "Тортила",
+      "Тротила"
+      ],
+      rightAnswer: {
+        2:true
       }
-        else{
-        answerText=answerText+": Верно! \n";
-        ++score;
-        }
-       }
+    },
+    {
+      question : "Спутник земли",
+      answer   : [
+      "Луна",
+      "Марс",
+      "Солнце"
+      ],
+      rightAnswer: {
+        1:true
+      }
+    },
+    {
+      question : "Крокодил друг чебурашки",
+      answer   : [
+      "Генадий",
+      "Ганджубас",
+      "Генератор"
+      ],
+      rightAnswer: {
+        1:true,
+      },
+    }
 
-   answerText=answerText+"\nВсего правильных ответов: "+score+"\n";
+  ];
 
-   alert(answerText);
-   yourAns = [];
-   score = 0;
-   clearForm("quiz");
-}
-function clearForm(name) {
-   var f = document.forms[name];
-   for(var i = 0; i < f.elements.length; ++i) {
-    if(f.elements[i].checked)
-        f.elements[i].checked = false;
-}
+  localStorage.setItem('data', JSON.stringify(info));
+
+  var questions = JSON.parse(localStorage.getItem('data')); 
+  
+  var htmlQuestion = $('#questionsGenerate').html();
+  var content = tmpl(htmlQuestion, {
+  data: questions
+  });
+  $('.questions').append(content);
+
+
+
+
+
+
+
+function checkAnswer(e) {
+  e.preventDefault();
+
+    var error = false;
+    
+    var user = [];
+    
+    for (var i = 0; i < questions.length; i++) {
+        
+      var inputs = $('.box' + i +' input:checkbox');
+            
+      var userAnswered = {};
+        for (var k = 0; k < inputs.length; k++) { 
+           var checked = inputs[k].checked;
+       
+           var right = questions[i].rightAnswer[k+1] == true;
+        
+           if (checked !== right) {
+            userAnswered[k]=false;
+               error = true;
+                
+           } else {
+            userAnswered[k]=true;
+            };
+    };
+    
+        user.push(userAnswered);
+      
+  };
+        
+
+      
+      function modalWindow () {
+        var $modal = $('<div class="modalWindow animated bounceInDown"><h2 class="text-center">Результат теста ' + (error? 'негативный!' : 'положительный!' )+'</h2></div>');
+        var $overlay = $('<div class="modalWindow-overlay"></div>');
+        
+        $('body').append($modal);
+        $('body').append($overlay);
+        
+        var htmlQuestion = $('#questionsGenerate').html();
+      
+      var content = tmpl(htmlQuestion, {
+        data: questions
+      });
+      
+            
+      $('.modalWindow').append(content);
+      var $buttonOk = $('<button class="btn btn-md btn-success" style="float:right">OK!</button>');
+      
+      $('.modalWindow').append($buttonOk);
+
+        for (var i = 0; i < questions.length; i++) {
+          var inputs = $('.box' + i +' input:checkbox');
+          var inputsShowResult =  $('.modalWindow .box' + i +' input:checkbox');
+          
+          for (var k = 0; k < questions[i].answer.length; k++) {
+            
+            var checked = inputs[k].checked;
+          
+          if ((checked == true)) {
+            if ((user[i][k]) == true) {
+              $(inputsShowResult[k]).attr({
+                "disabled":true,
+                "checked" : true  
+                }).parent().append("<span> Правильный ответ!</span>").find("span").css({"color" : "green"});
+          } else {
+            $(inputsShowResult[k]).attr({
+                "disabled":true,
+                "checked" : true  
+                }).parent().append("<span> Неправильный ответ!</span>").find("span").css({"color" : "red"});  
+
+          }; 
+
+          } else {
+            $(inputsShowResult[k]).attr({
+                "disabled":true,
+                "checked" : false 
+          });
+          };
+
+        };
+        };
+
+        $overlay.one('click', hideModal);
+        $buttonOk.one('click', hideModal);
+
+        function hideModal() {
+      $modal.removeClass('bounceInDown').addClass('hinge');
+      setTimeout(function(){$modal.remove();});
+      $overlay.remove();
+    };
+
+      };
+
+      modalWindow();
+
 };
+
+ 
+
+
+$('.check').on('click', checkAnswer);
+
+
+});
 
