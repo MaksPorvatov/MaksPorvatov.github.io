fdb3a885903c1f9442e43737e8c1ce881571dcfa
Maks Porvatov <maksporvatov@gmail.com>
2016-03-30 11:11:22 +0300

    up

--- GoIT/markup/home/JS/LessonJS 23-24/js/app.js
+++ GoIT/markup/home/JS/LessonJS 23-24/js/app.js
@@ -1,21 +1,21 @@
 requirejs.config({
   paths: {
     'jquery': 'http://code.jquery.com/jquery-1.11.3.min',
-    'template': 'template'
+    'tmpl': 'tmpl'
   },
   shim: {
     'jquery': {
       exports: 'jQuery'
     },
-    'template': {
-      exports: 'template'
+    'tmpl': {
+      exports: 'tmpl'
     }
   }
 });
 
 require(
-  ['jquery', 'template', 'model', 'view', 'controller'],
-  function(jquery, template, model, view, controller) {
+  ['jquery', 'tmpl', 'model', 'view', 'controller'],
+  function(jquery, tmpl, model, view, controller) {
 
     var firstToDoList = ['Make coffee', 'Go to bed', 'Learn', 'Nothing'];
     var model = new model(firstToDoList);
