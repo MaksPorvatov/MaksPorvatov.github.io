90b1ee185e1a06f404f3bb802f778433aac44254
Maks Porvatov <maksporvatov@gmail.com>
2016-03-30 11:59:06 +0300

    up

--- GoIT/markup/home/JS/LessonJS 23-24/js/app.js
+++ GoIT/markup/home/JS/LessonJS 23-24/js/app.js
@@ -18,8 +18,8 @@ require(
   function(jquery, tmpl, Model, View, Controller) {
 
     var firstToDoList = ['JavaScript', 'HTML', 'CSS', 'jQuery'];
-    var model = new Model(firstToDoList);
-    var view = new View(model);
-    var controller = new Controller(model, view);
+    var model = new model(firstToDoList);
+    var view = new view(model);
+    var controller = new controller(model, view);
   }
 );
